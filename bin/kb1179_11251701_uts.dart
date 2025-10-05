7c0c3e688b150aaa475aaa8eb86368206b1439c7
mahesa60 <maheshasuro60@gmail.com>
2025-10-05 10:50:52 +0700

    initial uts

---
+++ bin/kb1179_11251701_uts.dart
@@ -0,0 +1,5 @@
+import 'package:kb1179_11251701_uts/kb1179_11251701_uts.dart' as kb1179_11251701_uts;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kb1179_11251701_uts.calculate()}!');
+}
---
+++ test/kb1179_11251701_uts_test.dart
@@ -0,0 +1,8 @@
+import 'package:kb1179_11251701_uts/kb1179_11251701_uts.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
